33eb0086c771830774d5cb39debf2961bdf69753
HappyMunch <chadbayquen@gmail.com>
2021-08-16 19:53:34 +0800

    Create My Account.php

---
+++ MyAccount/My Account.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<title>My Account</title>
+<head>
+
+<link rel="stylesheet" href="w3.css">
+<style>
+body, h1, h2, h3, h4, h5, h6  {
+  font-family: Claytonia, cursive ;
+}
+</style>
+
+<body style="background-color: #efd19f; background-size: cover">>
+<div style="background-color: #C6893F; height: 50px; margin: -23px -8px 0px -8px; "></div>
+<div style="float: right; background-color: black; width: 130px; height: 250px; border-radius: 80px; margin: -110px 100px 0px -8px; "></div>
+<div style="float: right; background-color: white; border-radius: 50px; margin: 20px -106px 0px 0px; "><a href="TheTeaCulture.php"><img src="Staff/TEA CULTURE LOGO 9.png" width="100px"></a></div>
+<br><br><br><br>
+<div class="w3-container">
+<div class="w3-center"><h1>Welcome to My Account<h1>
+<p style="font-size:75%; font-family:Questrial;">Select the appropriate button for you!<p>
+
+<form style="font-size:55%; font-family:Verdana;" method = "GET" action = "Staff/Login.php">
+<input type=submit value = "Staff">
+
+<form method = "GET" action = "Customer/Customer.php">
+<input type=submit value = "Customer">
+<br>
+
+<div style="float: center; background-color: efd19f; margin: 20px 0px 0px 0px; "><img src="teaculture_cover (2).jpg" width="800px"></div>
+
+</div>
+</div>
+</form> 
+</body>
+</html>

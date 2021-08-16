0f5e06d82cebec95e2c7cd802f6cdb4aee1d73d8
HappyMunch <chadbayquen@gmail.com>
2021-08-16 19:52:39 +0800

    Create Error_Page.php

---
+++ MyAccount/Staff/Error_Page.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>My Account</title>
+<head>
+
+<body style="background-color:d3d3d3">
+<link rel="stylesheet" href="w3.css">
+
+<div class="w3-container w3-amber">
+<div class="w3-center"><h1>An error has occured please return to My Account page<h1>
+<br>
+<a href = "../My Account.php">Return to My Account</a>
+
+</div>
+</div>
+</form> 
+</body>
+</html>
